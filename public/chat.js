dd5fb000a0a9b19882819a3b351e09f7e5b74c28
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-09-15 12:00:08 -0500

    [Feature] socket emit

--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,21 @@
 const socket = io()
+
+// DOM elements
+let message = document.getElementById('message')
+let username = document.getElementById('username')
+let btn = document.getElementById('send')
+let output = document.getElementById('output')
+let actions = document.getElementById('actions')
+
+btn.addEventListener('click', function () {
+    socket.emit('chat: message', {
+        message: message.value,
+        username: username.value,
+    })
+})
+
+socket.on('chat: message', function (data) {
+    output.innerHTML = `<p>
+        <strong>${data.username}</strong>: ${data.message}
+    </p>`
+})
