ab9c81c18f53b60ffc0ab71f44bdbc8186aa9baa
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-09-15 10:49:52 -0500

    [Add] server running

---
+++ index.js
@@ -0,0 +1,14 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+// settings
+app.set('port', process.env.PORT || 3000)
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+// start the server
+app.listen(app.get('port'), () => {
+    console.log('server on port', app.get('port'))
+})
