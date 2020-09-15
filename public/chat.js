039df146756cbc1fe19cc20a92bae594498fa39f
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-09-15 12:38:47 -0500

    [Fix] name of sockets

--- public/chat.js
+++ public/chat.js
@@ -8,14 +8,23 @@ let output = document.getElementById('output')
 let actions = document.getElementById('actions')
 
 btn.addEventListener('click', function () {
-    socket.emit('chat: message', {
+    socket.emit('chat:message', {
         message: message.value,
         username: username.value,
     })
 })
 
-socket.on('chat: message', function (data) {
-    output.innerHTML = `<p>
+message.addEventListener('keypress', function () {
+    socket.emit('chat:typing', username.value)
+})
+
+socket.on('chat:message', function (data) {
+    actions.innerHTML = ''
+    output.innerHTML += `<p>
         <strong>${data.username}</strong>: ${data.message}
     </p>`
+})
+
+socket.on('chat:typing', function (data) {
+    actions.innerHTML = `<p><em>${data} is typing a message</em></p>`
 })
