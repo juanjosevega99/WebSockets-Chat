de7a37427a810c853860b73e71894c8c8d91610f
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-09-15 11:12:35 -0500

    [Feature] first server ready connection between socket

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ app.set('port', process.env.PORT || 3000)
 // static files
 app.use(express.static(path.join(__dirname, 'public')))
 
-// start the server
-app.listen(app.get('port'), () => {
+// start the server 
+const server = app.listen(app.get('port'), () => {
     console.log('server on port', app.get('port'))
+})
+
+const SocketIO = require('socket.io')
+const io = SocketIO(server)
+
+// websockets
+io.on('connection', (socket) => {
+    console.log('new connection', socket.id)
 })
